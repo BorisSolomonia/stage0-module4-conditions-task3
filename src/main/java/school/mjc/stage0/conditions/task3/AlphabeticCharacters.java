a9a52ffa1aefc43aff355cbe2dfc514fb583f7c5
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2022-09-19 18:43:52 +0400

    Update AlphabeticCharacters.java

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        char ch = character;
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+            System.out.println("Vowel");
+        else
+            System.out.println("Consonant");
     }
 }
